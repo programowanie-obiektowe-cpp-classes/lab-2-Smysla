5942b423320c6cff35513423e6b67f4be31aadb6
Smysla <148914532+Smysla@users.noreply.github.com>
2023-11-08 17:10:03 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -7,6 +7,11 @@ class ResourceManager
     public:
     ResourceManager() {}
     ResourceManager(const ResourceManager& r) :res{r.res} {}
+    ResourceManager& operator=(const ResourceManager& r)
+    {
+        res=r.res;
+        return *this;
+    }
     ~ResourceManager() {}
     double get() {return res.get();}
 

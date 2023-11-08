a77ed9e39bf7c443c1a99c5c9f55958ac45e6a59
Smysla <148914532+Smysla@users.noreply.github.com>
2023-11-08 20:30:17 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -6,7 +6,7 @@ class ResourceManager
 {
     public:
     ResourceManager() : res{new Resource} {}
-    ResourceManager(const ResourceManager& r) {res = new Resource; res=r.res;}
+    ResourceManager(const ResourceManager& r) : :res{new Resource} {res=r.res;}
     ResourceManager& operator=(const ResourceManager& r)
     {
         res = new Resource;
